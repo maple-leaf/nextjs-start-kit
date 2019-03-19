88f1fb91d45dafad76e20b47710e0c12dd76b96d
maple-leaf <tangjf.88@163.com>
2019-03-19 21:17:34 +0800

    chore: project setup with next.js + styled components + typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.div`
+    font-size: 24px;
+    color: #fff000;
+    box-shadow: 3px 0 0 5px rgba(0, 0, 0, .3);
+`;
+
+function Home() {
+  return <Title>Welcome Next.js!</Title>
+}
+
+export default Home
